ddc0b2e7991617afcd3cc45712507d1fb325dc12
DipanshuGurjar123 <dipanshugurjar049@gmail.com>
2026-06-18 11:16:38 +0530

    create a todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,67 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState("");
+  const [tasks, setTasks] = useState(() => {
+    const saved = localStorage.getItem("myTask");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem("myTask", JSON.stringify(tasks))
+  })
+
+  const addTask = () => {
+    if (task.trim() === "") {
+      return;
+    }
+
+    setTasks([...tasks, task]);
+    setTask("");
+  }
+
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
+    <div className="min-h-screen bg-slate-50 flex justify-center pt-16 px-4">
+      <div className="max-w-md w-full bg-white rounded-xl shadow-md p-6 h-fit border-slate-100">
+        <h1 className="text-3xl font-bold text-slate-800 mb-6 flex items-center gap-2">Todo List</h1>
+
+      <div className="flex gap-2 mb-6">
+        <input
+        type="text"
+        placeholder="Add a new task..."
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+        className="flex-grow border border-slate-200 rounded-lg px-4 py-2.5 text-sm text-slate-700 focus:outline-none focus:border-sky-500 focus:ring-sky-500 transition-all"
+      />
+      <button
+        onClick={addTask}
+        className="bg-sky-500 hover:bg-sky-600 text-white font-medium rounded-lg px-5 py-2.5 transition-colors shadow-sm"
         >
-          Count is {count}
-        </button>
-      </section>
+        Add
+      </button>
+      </div>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      <div className="mt-6 border-t border-slate-100 pt-4">
+        {tasks.length === 0 ? (
+          <p>Your todo list is empty!, put any task</p>
+        ) : (
+          <div className="bg-slate-50/50 rounded-xl p-2 border-slate-100 space-y-2">
+            {tasks.map((item, index) => (
+              <div key={index}
+              className="flex justify-between items-center bg-white border border-slate-200/60 px-4 py-3 rounded-lg shadow-sm hover:sky-200 transition-all group">
+                <div>
+                  <span className="w-2 h-2 rounded-full bg-sky-400"></span>
+                  <span className="text-sm font-medium text-slate-700">{item}</span>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
